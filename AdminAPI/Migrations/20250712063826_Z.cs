06ecd43456ea81ccecf0316ecd85fc5a55d702c4
anshbhandary <anshbhandary2002@gmail.com>
2025-07-26 11:40:37 +0530

    Initial commit of Visual Studio project

---
+++ AdminAPI/Migrations/20250712063826_Z.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AdminAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Z : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
